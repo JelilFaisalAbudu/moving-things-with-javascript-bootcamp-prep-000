9c48d02578367a5bedbf41462febf56075f3305d
Jelil Abudu <abudujelilfaisal@gmail.com>
2020-11-07 21:22:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
+fun
+
 
 const dodger = document.getElementById('dodger');
 
 document.addEventListener('keydown', function(key) {
   if (key.which === 37) {
-    moveLeft(dodger)
+    moveLeft(dodger);
   }
-  else if ()
+  else if (key.which === 39) {
+    moveRight(dodger);
+  }
 });
